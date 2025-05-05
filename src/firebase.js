5b2091a65999da692b0c43c2c1bf6b7cb88c990c
Joseph Bryan Ferrer <143077202+qjbferrer@users.noreply.github.com>
2025-05-05 10:33:26 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPHvAwhe5ubW6YXgcFg8eibcau-PttoPU",
+  apiKey: "",
   authDomain: "onionmap-4943d.firebaseapp.com",
   projectId: "onionmap-4943d",
   storageBucket: "onionmap-4943d.firebasestorage.app",
